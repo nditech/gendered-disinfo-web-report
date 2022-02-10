a1a8474244bc72ef38f5d8e39d36eb6ca0ceefb5
jeanromainroy <jroy@charitableanalytics.org>
2022-02-10 18:03:48 +0300

    discussion and conclusion rendered

---
+++ src/report/4-Discussion.svelte
@@ -0,0 +1,43 @@
+<script>
+
+    // properties
+    export let lang;
+
+    // import strings
+    import { discussion } from '../strings.json';
+
+</script>
+
+<section>
+
+    <h1 class="subtitle">{discussion['title'][lang]}</h1>
+
+    {#each Object.keys(discussion['paragraphs_1']) as paragraph_key}
+        <p>
+            {@html discussion['paragraphs_1'][paragraph_key][lang]}
+        </p>
+        <br>
+    {/each}
+
+    <ol>
+        {#each Object.keys(discussion['enumerations']) as paragraph_key}
+            <li>
+                {@html discussion['enumerations'][paragraph_key][lang]}
+            </li>
+        {/each}    
+    </ol>
+
+
+    {#each Object.keys(discussion['paragraphs_2']) as paragraph_key}
+        <p>
+            {@html discussion['paragraphs_2'][paragraph_key][lang]}
+        </p>
+        <br>
+    {/each}
+
+
+</section>
+
+<style>
+
+</style>
---
+++ src/report/5-Conclusion.svelte
@@ -0,0 +1,26 @@
+<script>
+
+    // properties
+    export let lang;
+
+    // import strings
+    import { conclusion } from '../strings.json';
+
+</script>
+
+<section>
+
+    <h1 class="subtitle">{conclusion['title'][lang]}</h1>
+
+    {#each Object.keys(conclusion['paragraphs']) as paragraph_key}
+        <p>
+            {@html conclusion['paragraphs'][paragraph_key][lang]}
+        </p>
+        <br>
+    {/each}
+
+</section>
+
+<style>
+
+</style>
